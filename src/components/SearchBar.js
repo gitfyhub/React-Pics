9471c11ff9d7c26bfa6306abd1b37c17c23a65d2
Faisal Younus <gitfyhub>
2020-01-05 22:16:26 -0600

    initial commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./App";
+
+class SearchBar extends React.Component {
+    render() {
+        return <div>Search Bar</div>
+    }
+}
+
+export default SearchBar;
