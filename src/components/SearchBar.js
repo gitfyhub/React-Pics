02d1c74c40629969c04f48c954fcfc63b0ffba48
Faisal Younus <gitfyhub>
2020-01-06 22:32:05 -0600

    added search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,23 @@
 import React from "react";
-import App from "./App";
 
 class SearchBar extends React.Component {
-    render() {
-        return <div>Search Bar</div>
-    }
-}
+  state = { term: "" };
 
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 export default SearchBar;
