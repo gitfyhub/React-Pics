985fc15d61ec4d5f6c545500a6d5da93ccfd6e35
Faisal Younus <gitfyhub>
2020-01-07 11:33:34 -0600

    added axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    // console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: {query: term},
+      headers: {
+        Authorization: 'Client-ID 3312454b615ea8801d4fb5f5d74a390541c0f758eeac1c4ee11aa5e6b84dafe5'
+      }
+    });
   }
 
   render() {
@@ -16,3 +23,7 @@ class App extends React.Component {
 }
 
 export default App;
+
+// 3312454b615ea8801d4fb5f5d74a390541c0f758eeac1c4ee11aa5e6b84dafe5
+
+// 9b2403b9680c66171111c5f4cc539a4ffb7db36ffc33bd695ab658665b1aadd9
